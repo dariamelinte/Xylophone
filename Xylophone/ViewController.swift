1c8ac588caaef0eac1cb653914a89a5e1ac8e513
daria melinte <dariamelinte2003@gmail.com>
2021-07-05 13:46:09 +0300

    create the functionality for playing sounds from different buttons

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func playSound(note: String) {
+        guard let url = Bundle.main.url(forResource: note, withExtension: "wav") else {
+            print("Could not find the sound needed")
+            return
+        }
 
+        do {
+            // make the sound play when the phone is on silent or when the phone has been blocked
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
 
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+
+            if let player = player {
+                player.play()
+            }
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
+    @IBAction func keyPressed(_ button: UIButton) {
+        if let note = button.currentTitle {
+            playSound(note: note);
+        }
+    }
+    
+    
     
 
 }
